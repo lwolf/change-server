aed0b400bc69dd2ae8176d65be56d98ae03ebce2
Sergey Nuzhdin <devnull@localhost>
2013-05-02 19:36:55 +0300

    first working version

--- js/options.js
+++ js/options.js
@@ -1,42 +1,12 @@
 
 function init(){
-    setSelectedValue("language", readProperty("language", "en_US"));
-    localize();
     renderItems();
-    doDataExchange(false);
-}
-
-function getSelectedLanguage(){
-    var lang = getSelectedValue("language");
-    return "lang_" + lang;
-}
-
-function onLanguageSelected(){
-    doDataExchange(true);
-    init();
+    localize();
 }
 
 function localize(){
-    var lang = getSelectedLanguage();
 
-    for (var ctrlId in lang){
-        var value = lang[ctrlId];
-
-        var ctrl = document.getElementById(ctrlId);
-
-        if (ctrl == null) {
-            continue;
-        }
-
-        if (ctrl.tagName == "SPAN"){
-            ctrl.innerText = value;
-        }else if (ctrl.tagName == "INPUT"){
-            ctrl.value = value;
-        }
-    }
 }
-
-
 function createOption(text, value){
 	var opt = document.createElement('option');
 
@@ -64,33 +34,58 @@ function setSelectedValue(selectId, value){
 	}
 }
 
-
-
-function doDataExchange(save){
-    if (save){
-        localStorage["language"] = getSelectedValue["language"];
-    }else{
-        setSelectedValue("language", readProperty("language", "en_US"))
-    }
-}
-
 function AddItem(){
-    var domain = document.getElementById('domain').value;
-    var alias = document.getElementById('alias').value;
+    var item_id = document.getElementById('item_id');
+    var domain = document.getElementById('domain');
+    var alias = document.getElementById('alias');
 
     var stored_items = JSON.parse(readProperty('items', '[]'));
+    if (!domain.value || !alias.value){
+        return
+    }else if (!item_id.value){
+        stored_items.push([domain.value, alias.value].toString());
+        localStorage['items'] = JSON.stringify(stored_items);
+        renderItem(domain.value, alias.value);
+    }else if (domain.value && alias.value){
+        for (var i = 0; i < stored_items.length; i++){
+            if (i == parseInt(item_id.value)){
+                stored_items[i] = [domain.value, alias.value].join(',');
+                renderItem(domain.value, alias.value, i);
+            }
+        }
 
-    stored_items.push([domain, alias].toString());
+    }
     localStorage['items'] = JSON.stringify(stored_items);
-    renderItem(domain, alias);
+    domain.value = "";
+    alias.value = "";
+    item_id.value = "";
+
 }
 
-function RemoveItem(item_id){
+function onRemove(){
     var stored_items = JSON.parse(readProperty('items', '[]'));
-    // remove
+    var item_id = parseInt(this.getAttribute('id').split('_')[1]);
+    var new_items_list = [];
+    for (var i = 0; i < stored_items.length; i++){
+        if (i != item_id){
+            new_items_list.push(stored_items[i]);
+        }
+    }
+    localStorage['items'] = JSON.stringify(new_items_list);
+    renderItems();
 }
 
-function EditItem(item_id){
+function onEdit(){
+    var stored_items = JSON.parse(readProperty('items', '[]'));
+    var item_id = parseInt(this.getAttribute('id').split('_')[1]);
+    for (var i = 0; i < stored_items.length; i++){
+        if (i == item_id){
+            var data = stored_items[i].split(',');
+            document.getElementById('item_id').value = i;
+            document.getElementById('domain').value = data[0];
+            document.getElementById('alias').value = data[1];
+        }
+    }
 }
 
 function renderItems(){
@@ -98,21 +93,38 @@ function renderItems(){
     var stored_items = JSON.parse(readProperty('items', '[]'));
     for (var i = 0; i < stored_items.length; i++){
         var parts = stored_items[i].split(',');
-        renderItem(parts[0], parts[1]);
+        renderItem(parts[0], parts[1], i);
     }
 
 }
 
-function renderItem(domain, alias){
+function renderItem(domain, alias, item_id){
+
     var list_of_items = document.getElementById("items");
+    var old_item = document.getElementById("item_" + item_id);
+    if (old_item){
+        list_of_items.removeChild(old_item);
+    }
+
+
     var new_item = document.createElement('li');
     var editButton = document.createElement('button');
     var removeButton = document.createElement('button');
+
+    new_item.setAttribute('id', "item_" + item_id);
     new_item.innerHTML = domain + " [" + alias + "]";
+
+    editButton.setAttribute('id', 'edit_' + item_id);
     editButton.innerHTML = 'Edit';
+    editButton.addEventListener('click', onEdit);
+
+    removeButton.setAttribute('id', 'remove_' + item_id);
     removeButton.innerHTML = 'Remove';
+    removeButton.addEventListener('click', onRemove);
+
     new_item.appendChild(editButton);
     new_item.appendChild(removeButton);
+
     list_of_items.appendChild(new_item);
 }
 
@@ -121,21 +133,16 @@ function onClear(){
     init();
 }
 
-function onSave(){
-    doDataExchange(true);
-    window.close()
-}
-
 function onExit(){
     window.close()
 }
 
 document.addEventListener( "DOMContentLoaded" , function () {
     init();
-    document.getElementById("language").addEventListener( "change" , onLanguageSelected);
-    document.getElementById("lngAdd").addEventListener( "click" , AddItem);
-    document.getElementById("lngSave").addEventListener( "click" , onSave);
-    document.getElementById("lngClear").addEventListener( "click" , onClear);
-    document.getElementById("lngExit").addEventListener( "click" , onExit);
+    document.getElementById("AddBtn").addEventListener("click", AddItem);
+    //document.getElementById("EditBtn").addEventListener("click", onEdit);
+    //document.getElementById("RemoveBtn").addEventListener("click", onRemove);
+    document.getElementById("ClearBtn").addEventListener("click", onClear);
+    document.getElementById("ExitBtn").addEventListener("click", onExit);
 });
 
